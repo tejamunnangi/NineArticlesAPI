2c564dc154a25f364ce642cd0787258467cee6e0
Ravi's PC <Ravi's PC@DESKTOP-K9N8NUI.home>
2019-11-15 23:21:24 +1100

    overriding equals and hashcode

--- src/main/java/com/nine/publishing/domain/Tag.java
+++ src/main/java/com/nine/publishing/domain/Tag.java
@@ -45,5 +45,30 @@ public class Tag {
 	public void setUpdatedDate(LocalDate updatedDate) {
 		this.updatedDate = updatedDate;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((tag == null) ? 0 : tag.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Tag other = (Tag) obj;
+		if (tag == null) {
+			if (other.tag != null)
+				return false;
+		} else if (!tag.equals(other.tag))
+			return false;
+		return true;
+	}
 	
 }
